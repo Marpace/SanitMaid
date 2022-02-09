f884fe447fdf3566e6fe79eed11142f2f813e2ef
Marpace <almonacid.pablo@hotmail.com>
2022-02-09 11:44:31 -0500

    update quote form

--- js/index.js
+++ js/index.js
@@ -149,67 +149,142 @@ nextBtn.addEventListener("click", function(){
         isAnimating = false;
     }, 400);
 });
-// quote form functions
+
+
+// quote form functions-----------------------------------------------
 
 const quoteItems = [...document.querySelectorAll(".quote-form-item")]
 const quoteButtons = [...document.querySelectorAll(".quote-form-item-button")]
 const iconText = [...document.querySelectorAll(".quote-icons-item-text")]
+const quoteTitles = document.querySelectorAll(".quote-form-item-title h2")
+
+const sizeText = document.querySelector(".size-text");
+const sizeButton = document.querySelector(".size-button");
+const sizeInput = document.querySelector("#size-input");
+
+
+const extrasButton = document.querySelector(".extras-button");
+const extrasInput = document.querySelectorAll(".extras-input");
+const extrasIconItem = document.querySelector(".extras-icon-item");
 
 
 let quoteIndex = 0;
 
 
 //slide content up and down when an option is clicked
-$(".quote-form-item-title").click(function(){
-    $(this).next().slideToggle();
-    $(this).parent().toggleClass("open");
-});
 
 
-$("#size-options").parent().prev().click(function(){
-    $("#size-input").focus();
-});
-// $("#bath-next-btn").click(function (){
-//     $("#size-input").focus();
-//     console.log("clicked")
+// $(".quote-form-item-title").click(function(){
+//     $(this).next().slideToggle();
+//     $(this).parent().toggleClass("open");
 // });
 
-let iconIndex = 0;
 
-if (screen.width <= 767){
-    $(".quote-form-item-button").click(function(){
-        $(this).parent().slideToggle();
-        $(this).parent().parent().next().children(".quote-form-item-wrap").slideToggle();
-        $(this).parent().parent().toggleClass("open");
-        $(this).parent().parent().next().toggleClass("open");
-    });
-}
-// } else {
-//     $(".quote-form-item-button").each(function(){
-//         $(this).click(function(){
-//             console.log($(this).parent().parent())
-//             $(this).parent().parent().css("opacity", "0")
-//             $(this).parent().parent().next().css("opacity", "1")
-//             $(".quote-icons-item-text").eq(iconIndex).fadeIn(600, "linear");
-//             iconIndex += 1;
-//         });  
-//     })
+// $("#size-options").parent().prev().click(function(){
+//     $("#size-input").focus();
+// });
+
+
+// if (screen.width <= 767){
+//     $(".quote-form-item-button").click(function(){
+//         $(this).parent().slideToggle();
+//         $(this).parent().parent().next().children(".quote-form-item-wrap").slideToggle();
+//         $(this).parent().parent().toggleClass("open");
+//         $(this).parent().parent().next().toggleClass("open");
+//     });
 // }
 
-if(screen.width >= 768) {
 
-    quoteButtons.forEach(button => {
-        button.addEventListener("click", function(){
-            const selectedValues = document.querySelectorAll('input[type="radio"]:checked');
+// Quote section functionality for screens 768px and bigger 
 
-            console.log(quoteItems[quoteIndex])
-            quoteItems[quoteIndex].style.opacity = "0";
-            quoteItems[quoteIndex].style.zIndex = "0";
-            quoteItems[quoteIndex + 1].style.opacity = "1";
-            quoteItems[quoteIndex + 1].style.zIndex = "10";
-            iconText[quoteIndex].style.opacity = "1";
-            iconText[quoteIndex].innerHTML = selectedValues[selectedValues.length - 1].nextElementSibling.innerHTML;
-            quoteIndex += 1;
-        });
+
+quoteButtons.forEach(button => {
+    button.addEventListener("click", function(){
+        let index = quoteButtons.indexOf(this);
+        const selectedValues = document.querySelectorAll('input[type="radio"]:checked');
+
+        // const selectedValues = $('input[type="radio"]:checked');
+        if(selectedValues.length === (index + 1)){
+            if(screen.width < 768){
+                $(this).parent().slideToggle();
+                $(this).parent().parent().next().children(".quote-form-item-wrap").slideToggle();
+                $(this).parent().parent().toggleClass("open");
+                $(this).parent().parent().next().toggleClass("open")
+                $(this).parent().prev().children("img").fadeIn(600, "linear")
+                this.parentElement.previousElementSibling.style.justifyContent = "space-between";
+                this.parentElement.previousElementSibling.children[1].innerHTML = selectedValues[index].nextElementSibling.innerHTML
+                console.log(selectedValues[index].nextElementSibling.innerHTML)
+            } else {
+                quoteItems[index].classList.toggle("fade-in")
+                quoteItems[index + 1].classList.toggle("fade-in")
+                iconText[index].style.opacity = "1";
+                iconText[index].innerHTML = selectedValues[index].nextElementSibling.innerHTML;
+            }
+            index += 1;
+        } else {
+            $(this).next().fadeIn(400, "linear")
+        }
     });
-}
+});
+// function for size section because of different input type 
+sizeButton.addEventListener("click", function(){
+    
+    if(sizeInput.value === ""){
+        $(this).next().fadeIn(400, "linear")
+    } else {
+        if(screen.width < 768) {
+            $(this).parent().slideToggle();
+            $(this).parent().parent().next().children(".quote-form-item-wrap").slideToggle();
+            $(this).parent().parent().toggleClass("open");
+            $(this).parent().parent().next().toggleClass("open")
+            $(this).parent().prev().children("img").fadeIn(600, "linear")
+            this.parentElement.previousElementSibling.style.justifyContent = "space-between";
+            this.parentElement.previousElementSibling.children[1].innerHTML = `${sizeInput.value} ft&sup2;`
+
+        } else {
+            quoteItems[3].classList.toggle("fade-in");
+            quoteItems[4].classList.toggle("fade-in");
+            quoteItems.splice(3, 1);
+            sizeText.style.opacity = "1"
+            sizeText.innerHTML = `${sizeInput.value} ft&sup2;`
+        }
+    }
+    
+});
+// function for extras section because of different input type 
+extrasButton.addEventListener("click", function(){
+    const checkedItems = document.querySelectorAll('input[type="checkbox"]:checked');
+
+    if(checkedItems.length > 0){
+        if(screen.width < 768) {
+            $(this).parent().slideToggle();
+            $(this).parent().parent().next().children(".quote-form-item-wrap").slideToggle();
+            $(this).parent().parent().toggleClass("open");
+            $(this).parent().parent().next().toggleClass("open")
+            $(this).parent().prev().children("img").fadeIn(600, "linear")
+            this.parentElement.previousElementSibling.style.justifyContent = "space-between";
+            this.parentElement.previousElementSibling.children[1].innerHTML = "";
+        } else {
+            quoteItems[3].classList.toggle("fade-in");
+            quoteItems[4].classList.toggle("fade-in");
+            quoteItems.splice(3, 1);
+        }
+        extrasInput.forEach(input => {
+            if (input.checked){
+                if(screen.width <  768) {
+                    const newEl = document.createElement("h2");
+                    newEl.innerHTML = input.nextElementSibling.innerHTML;
+                    this.parentElement.previousElementSibling.children[1].appendChild(newEl);
+                } else {
+                    const newEl = document.createElement("div");
+                    newEl.classList.add("extras-text");
+                    newEl.innerHTML = input.nextElementSibling.innerHTML;
+                    extrasIconItem.appendChild(newEl);
+                }
+            }
+        });
+    } else {
+        $(this).next().fadeIn(400, "linear")
+    }
+    
+});
