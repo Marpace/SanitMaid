c26dad1e71970133fd0a80a1866483889dd134f6
Marpace <almonacid.pablo@hotmail.com>
2022-03-08 10:48:08 -0500

    update services page

--- js/services.js
+++ js/services.js
@@ -1,10 +1,3 @@
-// $(".accordion-toggle").each(function(){
-//     $(this).click(function(){
-//         $(this).parent().next().slideToggle(600, "swing");
-//         console.log($(this).next())     
-//     });
-
-// })
 
 $(".accordion-toggle").click(function(){
 
@@ -20,21 +13,53 @@ $(".accordion-toggle").click(function(){
     //     $(this).parent().next().slideToggle(600, "swing");
     // }
 
+    // Opens accordion section 
     $(this).parent().next().slideToggle(600, "swing");
 
+    // toggles "+" sign into "-" 
     $(this).children(".toggle-2").toggleClass("accordion-open");
 
-    console.log($(".accordion-toggle"))
 });
 
+const rooms = document.querySelectorAll(".room");
 
-// $(".quote-form-item-title").click(function(){
-//     if($(this).next().css("display") === "block"){
-//         $(this).next().slideToggle();
-//     } else {
-//         $(".quote-form-item-wrap").each(function(){
-//             $(this).slideUp(); 
-//         })
-//         $(this).next().slideToggle();
-//     }
-// });
+rooms.forEach(room => {
+    room.addEventListener("click", function(){
+        const titleHeight = getComputedStyle(this.children[1]).height;
+
+        // check whether image is visible
+        if(getComputedStyle(this.children[0]).opacity === "1"){
+            // moves title to the top
+            this.children[1].style.top = "0";
+            this.children[1].style.transform = "translate(-50%, 0)"
+            
+            setTimeout(()=> {
+                // makes image disappear 
+                this.children[0].style.opacity = "0"
+    
+                // animates height of room div
+                this.style.maxHeight = "600px"
+    
+                //Adjust list padding according to title height
+                this.children[2].style.padding = `${titleHeight} 10px 10px`
+    
+            }, 400)
+        } else {
+            // makes image appear 
+            this.children[0].style.opacity = "1"
+
+            // animates height of room div
+            this.style.maxHeight = "300px"
+            
+            setTimeout(()=> {
+                // moves title to the center
+                this.children[1].style.top = "50%";
+                this.children[1].style.transform = "translate(-50%, -50%)"
+            }, 400)
+        }
+
+
+
+
+    });
+});
